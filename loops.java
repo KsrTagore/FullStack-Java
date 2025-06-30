f46842ee7aa1a701077365ed5f4b11f351a2e7fb
KsrTagore <ksrtagore7@gmail.com>
2025-06-30 22:01:47 +0530

    Create loops.java

---
+++ loops.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;More actions
+class loops{
+public static void main(String[] args){
+int i;
+Scanner s=new Scanner(System.in);
+/* 1). 
+for(i=0;i<10;i++){
+System.out.println("Bright IT Career");
+}
+*/
+/* 2).
+i=1;
+while(i<=20){
+System.out.println(i);
+i++;
+}*/
+/* 3).
+System.out.println("enter a:");
+int a=s.nextInt();
+@@ -35,6 +37,7 @@
+else{
+System.out.println(n+" is Odd");
+}*/
+/* 5).
+System.out.println("Enter a:");
+int a=s.nextInt();
+@@ -51,18 +54,21 @@
+else{
+System.out.println("c is large");
+}*/
+/* 6). 
+i=10;
+while(i<=100){
+System.out.println(i);
+i++;
+}*/
+/* 7).
+i=1;
+do{
+System.out.println(i);
+i++;
+}while(i<=10);*/
+/* 8).
+int sum=0,rem;
+System.out.println("enter n:");
+@@ -79,6 +85,7 @@
+else{
+System.out.println("Given number is not  Armstrong number");
+}*/
+/* 10).
+int rem=0,rev=0;
+System.out.println("enter n:");
+@@ -107,6 +114,7 @@
+case 1: System.out.println(n+" is odd");
+break;
+}*/
+/* 12).
+System.out.println("enter g:");
+String g=s.next();
+@@ -126,6 +134,7 @@
+default:
+System.out.println("Invalid");
+}*/
+/* 13).
+int a=10,b=20,c=30;
+if(a>b){
+@@ -139,7 +148,5 @@
+System.out.println(c+" is large");
+}*/
+}
+}
+}
